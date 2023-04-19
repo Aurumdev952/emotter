b7633abd0fb54a80a41e5091c47e9062ae139b3f
aurumdev952 <benjamugi20072@gmail.com>
2023-04-19 09:37:49 +0200

    added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,24 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import Layout from "~/components/Layout";
+
+
+const NotFound: NextPage = () => {
+    return (<>
+    <Head>
+    <title>Emotter - 404</title>
+    <meta name="description" content="page not found!" />
+    <link rel="icon" href="/emotter.svg" />
+    </Head>
+    <Layout>
+        <div className="w-full h-full flex flex-col justify-start items-center pt-24 gap-4">
+            <h1 className="font-bold text-white text-[7rem]">
+                404
+            </h1>
+            <p className="text-2xl">page not found</p>
+        </div>
+    </Layout>
+    </>)
+}
+
+export default NotFound
